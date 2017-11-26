affe3a02c2d76d9955275a19c0a7408014636ec2
Maria Pouliou <31806743+marypouliou@users.noreply.github.com>
2017-11-26 23:19:10 +0000

    Create mysql Connection

--- Bricks_Management.java
+++ Bricks_Management.java
@@ -1,12 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 
 public class Bricks_Management {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		getConnection();
 
 	}
 
+	
+	public static Connection getConnection() throws Exception{
+		//credentials
+		try{
+			String driver = "com.mysql.cj.jdbc.Driver";
+			String url = "jdbc:mysql://localhost:3307/CRM_DB";
+			String username = "root";
+			String password = "";
+			Class.forName(driver);
+			//establish the connection
+			Connection conn = DriverManager.getConnection(url, username, password);
+			return conn;
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		return null;
+	}
 }
