dbe9d4e44595bb87eeebaa4a83031c31d1d31f9c
Maria Pouliou <31806743+marypouliou@users.noreply.github.com>
2017-11-26 23:28:35 +0000

    Create Orders Table

--- Bricks_Management.java
+++ Bricks_Management.java
@@ -1,14 +1,26 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 
 public class Bricks_Management {
 
 	public static void main(String[] args) throws Exception {
-		getConnection();
+		createTable();
 
 	}
 
+	public static void createTable() throws Exception{
+		try{
+			Connection con = getConnection();
+			PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS Orders (Order_Reference_Number INT NOT NULL AUTO_INCREMENT,Customer_Surname varchar(20) NOT NULL,Customer_Name varchar(20) NOT NULL,Number_of_Bricks INT NOT NULL,Dispatched varchar(3),Dispatched_Date varchar(20),PRIMARY KEY (Order_Reference_Number))");
+			create.executeUpdate();
+			con.close();
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	
 	
 	public static Connection getConnection() throws Exception{
 		//credentials
